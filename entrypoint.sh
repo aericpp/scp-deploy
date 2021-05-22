24be7121282a6bf85cb0209104df113c98c39bec
aericpp <p.yue89@gmail.com>
2021-05-23 00:45:12 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,9 +10,4 @@ chmod 600 "$SSH_CONFIG_PATH/key"
 echo "$SSL_CLIENT_KEY" > "$SSH_CONFIG_PATH/ssl.key"
 echo "$SSL_CLIENT_PEM" > "$SSH_CONFIG_PATH/ssl.pem"
 
-scp $ARGS \
-  -i "$SSH_CONFIG_PATH/key" \
-  -o StrictHostKeyChecking=no \
-  -o User="$USERNAME" \
-  -r "$GITHUB_WORKSPACE/$FOLDER" \
-  $SERVER_IP:$SERVER_DESTINATION
+scp $ARGS -i "$SSH_CONFIG_PATH/key" -o StrictHostKeyChecking=no -o User="$USERNAME" -r "$GITHUB_WORKSPACE/$FOLDER" $SERVER_IP:$SERVER_DESTINATION
