18fcdf9d057acfaacdeea9c60253f17a816689db
aericpp <p.yue89@gmail.com>
2021-05-23 00:28:41 +0800

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -eu
+
+SSH_CONFIG_PATH="$HOME/.ssh"
+test -d "$SSH_CONFIG_PATH" || mkdir -p "$SSH_CONFIG_PATH"
+
+echo "$DEPLOY_KEY" > "$SSH_CONFIG_PATH/key"
+chmod 600 "$SSH_CONFIG_PATH/key"
+
+echo "$SSL_CLIENT_KEY" > "$SSH_CONFIG_PATH/ssl.key"
+echo "$SSL_CLIENT_PEM" > "$SSH_CONFIG_PATH/ssl.pem"
+
+scp $ARGS \
+  -i "$SSH_CONFIG_PATH/key" \
+  -o StrictHostKeyChecking=no \
+  -o User="$USERNAME" \
+  -r "$GITHUB_WORKSPACE/$FOLDER" \
+  $SERVER_IP:$SERVER_DESTINATION
