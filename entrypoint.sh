d0d00e5b70976df2fdad17c07735452b57fb6ce7
aericpp <p.yue89@gmail.com>
2021-05-23 14:41:27 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -eu
+pwd
 
 SSH_CONFIG_PATH="$HOME/.ssh"
 test -d "$SSH_CONFIG_PATH" || mkdir -p "$SSH_CONFIG_PATH"
@@ -7,7 +8,7 @@ test -d "$SSH_CONFIG_PATH" || mkdir -p "$SSH_CONFIG_PATH"
 echo -e "$DEPLOY_KEY" > $SSH_CONFIG_PATH/key
 chmod 600 $SSH_CONFIG_PATH/key
 
-echo -e "$SSL_CLIENT_KEY" > $SSH_CONFIG_PATH/ssl.key
-echo -e "$SSL_CLIENT_PEM" > $SSH_CONFIG_PATH/ssl.pem
+# echo -e "$SSL_CLIENT_KEY" > $SSH_CONFIG_PATH/ssl.key
+# echo -e "$SSL_CLIENT_PEM" > $SSH_CONFIG_PATH/ssl.pem
 
-scp $ARGS -i "$SSH_CONFIG_PATH/key" -o StrictHostKeyChecking=no -o User="$USERNAME" -r "$GITHUB_WORKSPACE/$FOLDER" $SERVER_IP:$SERVER_DESTINATION
+# scp $ARGS -i "$SSH_CONFIG_PATH/key" -o StrictHostKeyChecking=no -o User="$USERNAME" -r "$GITHUB_WORKSPACE/$FOLDER" $SERVER_IP:$SERVER_DESTINATION
