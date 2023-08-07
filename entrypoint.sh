b535a109810feea830bb45a685c3ce96ecb3cd2e
aericpp <p.yue89@gmail.com>
2023-08-07 11:24:39 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -eu
+echo "pwd: "
 pwd
 
 SSH_CONFIG_PATH="$HOME/.ssh"
@@ -22,6 +23,8 @@ cat "$SSH_CONFIG_PATH/config"
 
 touch $SSH_CONFIG_PATH/known_hosts
 chmod 644 $SSH_CONFIG_PATH/known_hosts
+
+echo "GITHUB_WORKSPACE: $GITHUB_WORKSPACE"
 
 ssh -F $SSH_CONFIG_PATH/config $SERVER_IP "rm -rf $SERVER_DESTINATION/*"
 scp -O -F $SSH_CONFIG_PATH/config -r $GITHUB_WORKSPACE/* $SERVER_IP:$SERVER_DESTINATION
