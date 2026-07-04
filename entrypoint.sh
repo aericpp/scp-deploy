7074a873f633da00adda98381f8651b13164a62f
aericpp <5396305+aericpp@users.noreply.github.com>
2026-07-04 15:47:33 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,8 +16,8 @@ echo "Host *" > "$SSH_CONFIG_PATH/config"
 echo "  User $USERNAME" >> "$SSH_CONFIG_PATH/config"
 echo "  StrictHostKeyChecking no" >> "$SSH_CONFIG_PATH/config"
 echo "  IdentityFile $SSH_CONFIG_PATH/key" >> "$SSH_CONFIG_PATH/config"
-echo "  ProxyCommand $ARGS" >> "$SSH_CONFIG_PATH/config"
-echo "  ForwardAgent yes" >> "$SSH_CONFIG_PATH/config"
+# echo "  ProxyCommand $ARGS" >> "$SSH_CONFIG_PATH/config"
+# echo "  ForwardAgent yes" >> "$SSH_CONFIG_PATH/config"
 
 cat "$SSH_CONFIG_PATH/config"
 
